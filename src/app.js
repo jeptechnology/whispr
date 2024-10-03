220acbf7e0a5a2269a9a01d87efc7b1d86fee0e2
James Parker <james@jeptechnology.com>
2024-10-03 08:12:16 +0100

    Fix start and end parsing

--- src/app.js
+++ src/app.js
@@ -74,51 +74,102 @@ webserver.listen(port, () => {
    console.log(`Server is running on port ${port}`);
    });
 
+
+function validateQueryParams(req)
+{
+   // Gather the query parameters here...
+   const component = req.query.component;
+   const severity = req.query.severity;
+   const file = req.query.file;
+   let start = req.query.start;
+   let end = req.query.end;
+
+   if (start)
+   {
+      start = Date.parse(start);
+      if (isNaN(start))
+      {
+         return { status: 400, message: 'Invalid start time: ' + start };
+      }
+   }
+
+   if (end)
+   {
+      end = Date.parse(end);
+      if (isNaN(end))
+      {
+         return { status: 400, message: 'Invalid end time: ' + end };
+      }
+   }
+
+   // if a component is specified, check if it exists
+   if (component)
+   {
+      if (global.log_structure.components[component] === undefined)
+      {
+         return { status: 400, message: 'Component not found' };
+      }
+   }
+
+   // if a severity is specified, check if it exists
+   if (severity)
+   {
+      if (global.log_structure.severity[severity] === undefined)
+      {
+         return { status: 400, message: 'Severity not found' };
+      }
+   }
+
+   // if a file is specified, check if it exists
+   if (file)
+   {
+      if (global.log_structure.files[file] === undefined)
+      {
+         return { status: 400, message: 'File not found' };
+      }
+   }
+
+   return { status: 200, message: 'OK' };
+}
+
 // Define the API routes
 // GET /api/log?component=component&severity=severity&file=file?start=start&end=end
 webserver.get('/api/log', (req, res) => {
    
    // Gather the query parameters here...
+   const response = validateQueryParams(req);
+   if (response.status !== 200)
+   {
+      res.status(response.status).json({ message: response.message });
+      return;
+   }
+
    const component = req.query.component;
    const severity = req.query.severity;
    const file = req.query.file;
-   const start = req.query.start;
-   const end = req.query.end;
+   let start = req.query.start;
+   let end = req.query.end;
+
+   // convert start and end to unix timestamps
+   if (start) start = Date.parse(start);
+   if (end) end = Date.parse(end);
 
    // create a set of all possible indeces in the log map - this will be 0..n-1 where n is the number of log entries
    let log_indeces = new Set();
    for (let i = 0; i < global.log_structure.entries.length; i++) log_indeces.add(i);
 
-   // if a component is specified, filter the log_indeces to only those that match the component
    if (component)
    {
-      if (global.log_structure.components[component] === undefined)
-      {
-         res.status(400).json({ message: 'Component not found' });
-         return;
-      }
       log_indeces = log_indeces.intersection(global.log_structure.components[component]);
    }
 
-   // if a severity is specified, filter the log_indeces to only those that match the severity
    if (severity)
    {
-      if (global.log_structure.severity[severity] === undefined)
-      {
-         res.status(400).json({ message: 'Severity not found' });
-         return;
-      }
       log_indeces = log_indeces.intersection(global.log_structure.severity[severity]);
    }
 
-   // if a file is specified, filter the log_indeces to only those that match the file
    if (file)
    {
-      if (global.log_structure.files[file] === undefined)
-      {
-         res.status(400).json({ message: 'File not found' });
-         return;
-      }
       log_indeces = log_indeces.intersection(global.log_structure.files[file]);
    }
 
@@ -126,11 +177,11 @@ webserver.get('/api/log', (req, res) => {
    logText = '';
    log_indeces.forEach((index) => {
       // check we are beyond start and before end
-      if (start && global.log_structure.entries[index].unixtimestamp < start)
+      if (start && (global.log_structure.entries[index].unixtimestamp < start))
       {
          return;
       }
-      if (end && global.log_structure.entries[index].unixtimestamp > end)
+      if (end && (global.log_structure.entries[index].unixtimestamp > end))
       {
          return;
       }
