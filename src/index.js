e17ad2fceba55d46cfb425b2b95e748f95fdf572
James Parker <james@jeptechnology.com>
2024-10-07 00:45:02 +0100

    WIP - protobuf decoding

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('node:path');
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
 
-webserver = require('./app');
+webserver = require("./app");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
@@ -14,31 +14,31 @@ const createWindow = () => {
     width: 640,
     height: 480,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
     },
     autoHideMenuBar: true,
     useContentSize: true,
-    icon: path.join(__dirname, 'public', 'images', 'icon.ico'),
+    icon: path.join(__dirname, "public", "images", "icon.ico"),
   });
 
   // and load the index.html from the express webserver
-  mainWindow.loadURL('http://localhost:3000/');
+  mainWindow.loadURL("http://localhost:3000/");
   // mainWindow.loadFile(path.join(__dirname, 'index.html'));
   mainWindow.focus();
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', () => {
+app.on("ready", () => {
   createWindow();
 
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
@@ -48,11 +48,61 @@ app.on('ready', () => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+
+// NOTE: The following code is in reference to docker's logdriver plugin API
+// https://github.com/docker/go-docker/blob/master/api/types/plugins/logdriver/io.go
+
+const protobuf = require("protobufjs");
+const fs = require("fs");
+
+function ReadBigEndian32bit(reader) {
+  const buffer = reader.buf;
+  const offset = reader.pos;
+  const value = buffer[offset] << 24 | buffer[offset + 1] << 16 | buffer[offset + 2] << 8 | buffer[offset + 3];
+  reader.pos += 4;
+  return value;
+}
+
+protobuf.load(__dirname + "/proto/dockerlogs.proto", function (err, root) {
+  if (err) throw err;
+
+  // Obtain a message type
+  var LogEntry = root.lookupType("LogEntry");
+
+  // Load the file /proto/test.log
+  var myFileContents = fs.readFileSync(__dirname + "/proto/container.log");
+  var reader = protobuf.Reader.create(myFileContents);
+
+  while (reader.pos < reader.len) {
+    
+    const length = ReadBigEndian32bit(reader);
+
+    // now that we have the length of this log line, make a note of the current position, and then decode the message
+    const oldReaderLength = reader.len;
+    reader.len = reader.pos + length;
+    var message = LogEntry.decode(reader);
+    reader.len = oldReaderLength;
+
+    // We should expect to see the same length as we read in the first 4 bytes
+    if (length != ReadBigEndian32bit(reader)) {
+      console.log(
+        "Length mismatch: " + length + " != " + lengthCheck + " at position " + reader.pos
+      );
+      break;
+    }
+
+    // get timestamp in nanoseconds
+    const timestamp = message.timeNano;
+    const date = new Date(timestamp / 1000000);
+    console.log(date.toISOString() + " " + message.line);
+  }
+});
--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,37 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+const protobuf = require("protobufjs");
+
+protobuf.load(__dirname + "/proto/dockerlogs.proto", function(err, root) {
+   if (err)
+       throw err;
+
+   // Obtain a message type
+   var LogEntry = root.lookupType("LogEntry");
+
+   // Exemplary payload
+   var payload = { 
+      source: "stout",
+      time_nano: "12345",
+      line: "Hello, World!"
+   };
+
+   // Verify the payload if necessary (i.e. when possibly incomplete or invalid)
+   var errMsg = LogEntry.verify(payload);
+   if (errMsg)
+       throw Error(errMsg);
+
+   // Create a new message
+   var message = LogEntry.create(payload); // or use .fromObject if conversion is necessary
+
+   // Encode a message to an Uint8Array (browser) or Buffer (node)
+   var buffer = LogEntry.encode(message).finish();
+   // ... do something with buffer
+
+   // Decode an Uint8Array (browser) or Buffer (node) to a message
+   var message = LogEntry.decode(buffer);
+   // ... do something with message
+
+   // If the application uses length-delimited buffers, there is also encodeDelimited and decodeDelimited.
+});
