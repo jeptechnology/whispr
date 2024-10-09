2cff6cb2b8c8f2723a4b3aacaf1781c9a8761c13
James Parker <james@jeptechnology.com>
2024-10-09 20:21:58 +0100

    JSON viewer

---
+++ app/(main)/pages/file_viewer/JsonViewer.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useContext } from "react"; 
+import { SupportPackageContext } from "../../../api/SupportPackage";
+import ReactJson from '@microlink/react-json-view'
+
+// define the JsonViewer component props
+export interface JsonViewerProps {
+    filename?: string;
+}
+
+
+const JsonViewer = (props: JsonViewerProps ) => {
+
+    const { files } = useContext(SupportPackageContext);
+
+    function ParseJsonFile(): any {
+        if (!props.filename) {
+            return {};
+        }
+        const content = files.get(props.filename);
+        if (content === undefined) {
+            return {};
+        }
+        return JSON.parse(content);
+    }
+
+    return props.filename ? 
+    (
+        <ReactJson 
+            src={ParseJsonFile()} 
+            theme="monokai"
+            iconStyle="circle"
+            indentWidth={4}
+            displayDataTypes={false}
+            onAdd={false}
+            onEdit={false}
+            onDelete={false}
+            collapsed={3}
+        />
+    ) : (
+        <div/>
+    );
+};
+
+export default JsonViewer;
--- app/(main)/pages/file_viewer/page.tsx
+++ app/(main)/pages/file_viewer/page.tsx
@@ -4,7 +4,8 @@ import React, { use } from "react";
 import { useContext, useState, useEffect } from "react"; 
 import { SupportPackageContext } from "../../../api/SupportPackage";
 import { Dropdown } from "primereact/dropdown";
-import LogViewer from "./LogViewer";
+import LogViewer from "./LogViewer"; // For log files
+import JsonViewer from "./JsonViewer"; // For json files
 
 const FileViewer = () => {
 
@@ -26,9 +27,13 @@ const FileViewer = () => {
             <div className="col-12">
                 <Dropdown value={selectedFile} options={options} placeholder="Select a file to view" onChange={onFileSelect} />
             </div>
-            
             <div className="col-12" style={{ height: "calc(100vh - 200px)" }}>
-                <LogViewer filename={selectedFile}/>
+                { selectedFile && selectedFile.endsWith(".log") ? 
+                (
+                  <LogViewer filename={selectedFile}/>
+                ) : (
+                  <JsonViewer filename={selectedFile}/>
+                ) }
             </div>            
         </div>
     );
